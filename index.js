3f05ea9775ef5d245377f2447a61f85bcdbde9ed
siprarc <75106097+siprarc@users.noreply.github.com>
2024-01-31 17:54:41 -0500

    Login testing with various scenarios

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.0/firebas
 
 // Add Firebase products that you want to use
   import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile} from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js'
-  import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js'
-
+  import { getFirestore, doc, getDoc, setDoc } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js'
+ 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -20,7 +20,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
-
+//Create a reference to db in firebase
+const db = getFirestore(app);
 //Need separate function that get info for email, password; Hint: Object destructuring 
 const getUserInfo = (emailId, passwordId,userNameId) => {
 const email = document.getElementById(emailId).value
@@ -37,7 +38,7 @@ userName,
 }
 //Need separate function for error handling
 //For Sign-up
-const signUpUser = () => {
+const signUpUser = async() => {
 const {
 email,password,userName  
 } = getUserInfo("sign-up-email","sign-up-password","user_name")
@@ -45,21 +46,31 @@ email,password,userName
 //const emailSignUp = document.getElementById("sign-up-email").value
 //const passwordSignUp = document.getElementById("sign-up-password").value
 console.log(email, password)
+//if username is not unique - Give a message to user saying the username exists
+const docRef = doc(db, "usernames",userName);
+const docSnap = await getDoc(docRef);
+
+if (docSnap.exists()) {
+  //this is the code if the username exists in the db    
+  console.log("Document data:", docSnap.data());
+  alert("Username is already in use")
+} else {
 createUserWithEmailAndPassword(auth, email, password)
 .then((userCredential) => {
-
 // Signed up 
 updateProfile(auth.currentUser, {
 displayName: userName, 
-}).then(() => {
+}).then(async() => {
 console.log("Profile Success"
-)  
+) 
+//Add username collection to firebase 
 // Profile updated!
-// ...
+await setDoc(doc(db, "usernames",userName), {});
 }).catch((error) => {
 console.log(error)
 // An error occurred
-// ...
+alert(error.message);
+console.log("Profile already exists in the database");
 });
   console.log("Success")
 //This is where we make the call for User with User name
@@ -68,7 +79,12 @@ console.log(error)
   const errorCode = error.code;
   const errorMessage = error.message;
   console.log(errorMessage)
+  alert(errorMessage)
 })
+  // docSnap.data() will be undefined in this case
+  console.log("No such document!");
+}
+
 }
 //For Sign-In
 const signInUser = () => {
