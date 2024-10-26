9ea899d9a992f5a0f3000ac0d76426f9003e0f24
siprarc <75106097+siprarc@users.noreply.github.com>
2024-10-26 13:32:17 -0400

    Displayed Poetry comments on pg

--- poetry.js
+++ poetry.js
@@ -1,37 +1,63 @@
+import { collection, getDocs, getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js'
+const firebaseConfig = {
+   apiKey: "AIzaSyCpdvnxHWnW3eBH8xyEHuv4zpcekNy84Lw",
+   authDomain: "sipra-s-page.firebaseapp.com",
+   projectId: "sipra-s-page",
+   storageBucket: "sipra-s-page.appspot.com",
+   messagingSenderId: "639310346557",
+   appId: "1:639310346557:web:a22a5d48cba0e8e848ad54",
+   measurementId: "G-LSCME9E44X"
+ };
+const app = initializeApp(firebaseConfig);
+//const auth = getAuth();
+const db = getFirestore(app);
+
+
 const comment1 = {
-    user_id: 'Siprarc',
+    userName: 'Siprarc',
     date: '8/8/2023',
     comment: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
 }
 const comment2 = {
-    user_id: 'Bianca',
+   userName: 'Bianca',
     date: '8/8/2023',
     comment: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
 }
 const comment3 = {
-    user_id: 'Anshur',
+   userName: 'Anshurp',
     date: '8/8/2023',
     comment: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
 }
 console.log('poetry')
 
-
+// next steps (written by bianca)
+// import everything you need from firebase
+// use comment.js as a guide for what you need
 
 //Loop through an array of object//
 const poetryCommentDiv = document.getElementById('poetry_comments')
-const poetryComments = [comment1,comment2,comment3]
+//const poetryComments = [comment1,comment2,comment3]
 //Line 22 will be deleted and will be pulledd from Firebase
-console.log(poetryComments)
-poetryComments.forEach(poetryComment => {
-   console.log(poetryComment) 
+const poetryComments = []
 
+
+const querySnapshot = await getDocs(collection(db, "comments_blog1"));
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+   console.log(doc.id, " => ", doc.data());
+   poetryComments.push(doc.data())
+});
+
+poetryComments.forEach(poetryComment => {
+   console.log(poetryComment)   
 //Create element for each comment
    const user_idContainer = document.createElement('p')
    const dateContainer = document.createElement('p')
    const commentContainer = document.createElement('p')
 
 // add text to each element
-   user_idContainer.innerHTML = poetryComment.user_id
+   user_idContainer.innerHTML = poetryComment.userName
    dateContainer.innerHTML = poetryComment.date
    commentContainer.innerHTML = poetryComment.comment
    
