0a77ab3b1c11e300cd128b2ee4f38b77b02b241d
siprarc <75106097+siprarc@users.noreply.github.com>
2024-12-02 21:55:42 -0500

    Updated content for different type of page -Art_work and category of Art-work

--- content.js
+++ content.js
@@ -1,9 +1,24 @@
+// Instructions for adding contnt
+// Create two divs with ids for titleContent, and content
+// buttons are there for content
+// the onclick should call showContent with the type and category
+// in content.js we need objects with content 
+// for the categories (one for the titles, one for the content).
+// update `contentObject` with the new category key and title and content objects
+
+
 //Show Content from Art work 
 const titleContentForArtWork = {
     PencilArt: '<p>Blog for Pencil Art</p>',
     PastelArt: '<p>Blog for Pastel Art</p>',
     RangoliArt: '<p>Blog for Rangoli Art</p>',
     AcrylicOilPaintings: '<p>Blog for Acrylic/Oil Paintings</p>',
+}
+const contentForArtWork = {
+    PencilArt: '<p>Content for Pencil Art</p>',
+    PastelArt: '<p>Content for Pastel Art</p>',
+    RangoliArt: '<p>Content for Rangoli Art</p>',
+    AcrylicOilPaintings: '<p>Content for Acrylic/Oil Paintings</p>',
 }
 //Show Content from Poetry 
 const titleContentForPoetry = {
@@ -35,14 +50,14 @@ const contentForPoetry = {
 	</div>
 </div>
     <p>
-		Silent flakes descend,❄️🌨️<br>
+		Silent flakes descend,<br>
 		Blanket of white adorns earth,<br>
 		Nature's hushed embrace.<br>
 		And Sun's gentle caress,<br>
 		Glistening snow in its embrace,<br>
-		Nature's sparkling kiss.😘<br>
+		Nature's sparkling kiss.<br>
 		Steam swirls above my noodle soup bowl,<br>
-		Snowflakes dance, slurping the warmth within.💕<br>	
+		Snowflakes dance, slurping the warmth within.<br>	
 	</p>`,
 
 
@@ -54,17 +69,41 @@ const contentForPoetry = {
     artThatSpeaks: '<p>Content for Art that speaks Poetry</p>',
     LifeAndCelebrations: '<p>Content for Life and Celebrations Poetry</p>',
 }
+const contentObject = {
+	poetry: {
+		titleContentObj:titleContentForPoetry,
+		contentObj:contentForPoetry
+	},
+	art: {
+		titleContentObj:titleContentForArtWork,
+		contentObj:contentForArtWork
+	},
+}
+
+
 const showContent = (type, category) => {
-    let titleContentObj={}
-    let contentObj={}
-    if (category === 'art') {
-        titleContentObj = titleContentForArtWork
-    } else if  (category === 'poetry') {
-        titleContentObj = titleContentForPoetry
-        contentObj = contentForPoetry
-    }
-
-
+    const titleContentObj=contentObject[category].titleContentObj
+    const contentObj=contentObject[category].contentObj
+    // if (category === 'art') {
+    //     titleContentObj = titleContentForArtWork
+    // } else if  (category === 'poetry') {
+    //     titleContentObj = titleContentForPoetry
+    //     contentObj = contentForPoetry
+    // 	}else if  (category === 'photography') {
+    //     	titleContentObj = titleContentForPhotography
+    //     	contentObj = contentForPhotography
+	// 		}else if  (category === 'food') {
+	// 			titleContentObj = titleContentForFood
+	// 			contentObj = titleContentForFood
+	// 			}else if  (category === 'drinks') {
+	// 				titleContentObj = titleContentForDrinks
+	// 				contentObj = titleContentForDrinks
+	// 				}else if  (category === 'flora_and_fauna') {
+	// 					titleContentObj = titleContentForFloraAndFauna
+	// 					contentObj = titleContentForFloraAndFauna
+	// 		
+console.log(titleContentObj[type])
+console.log(contentObj)			
 document.getElementById('titleContent').innerHTML = titleContentObj[type]
 document.getElementById('content').innerHTML = contentObj[type]
 }
